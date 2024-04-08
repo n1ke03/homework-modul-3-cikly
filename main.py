3f6a788f89402958a25894c45271673ff3ec58d1
Andrey Mikhailov <andreymikhailov03@gmail.com>
2024-04-08 13:49:29 +0300

    complite

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
-# num1 = int(input("Введите целое число: "))
-# num2 = int(input("Введите целое число: "))
+num1 = int(input("Введите целое число: "))
+num2 = int(input("Введите целое число: "))
 # for i in range(num1, num2):
 #   if i % 7 == 0:
+#     print(i)
+
+
+# for i in range(num1, num2):
+#   print(i)
+# for i in range(num1, num2, -1):
+#   print(i)
+# for i in range(num1, num2):
+#   if i % 7 == 0:
+#     print(i)
+# count = 0
+# for i in range(num1, num2):
+#   if i % 5 == 0:
+#     count += 1
+#     print(count)
+
+
+# for i in range(num1, num2):
+#   if i % 3 == 0 and i % 5 == 0:
+#     print("Fizz Buzz")
+#   elif i % 5 == 0:
+#     print("Buzz")
+#   elif i % 3 == 0:
+#     print("Fizz")
+#   elif i % 3 and i % 5:
 #     print(i)
